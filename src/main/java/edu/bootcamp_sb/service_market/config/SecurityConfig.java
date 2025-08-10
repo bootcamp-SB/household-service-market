904a6685559b0620797a666ee657044fa1dfd757
yasas-sri-udara <yasasmedagedara8@gmail.com>
2025-08-10 16:10:46 +0530

    custom exception for access denied

--- src/main/java/edu/bootcamp_sb/service_market/config/SecurityConfig.java
+++ src/main/java/edu/bootcamp_sb/service_market/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package edu.bootcamp_sb.service_market.config;
 
 
+import edu.bootcamp_sb.service_market.exception.CustomAccessDeniedHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
@@ -10,6 +11,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,15 +35,28 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfig->corsConfig.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(
+                            Collections.singletonList("http://localhost:3000")
+                        );
+                    corsConfiguration.setAllowedMethods(Collections.singletonList("**")); //pre-flight req
+                    corsConfiguration.setMaxAge(10L);
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                    return corsConfiguration;
+
+                }))
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("/api/v1/client").permitAll()
                                 .requestMatchers(freeSwaggerUrls).permitAll()
-                                .requestMatchers("/api/v1/**").authenticated()
+                                .requestMatchers("/api/v1/**").hasRole("ADMIN")
 
 
         );
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.exceptionHandling(exceptionHandlingConfig->
+                exceptionHandlingConfig.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
