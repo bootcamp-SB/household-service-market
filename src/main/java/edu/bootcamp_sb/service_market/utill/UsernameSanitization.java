b3eb34ba9c41d3f7804aebe3039379a5da5030f2
YasasSri2002 <srimedagedara234@gmail.com>
2026-01-03 13:53:14 +0530

    made Username Sanitization class constructor private

--- src/main/java/edu/bootcamp_sb/service_market/utill/UsernameSanitization.java
+++ src/main/java/edu/bootcamp_sb/service_market/utill/UsernameSanitization.java
@@ -4,6 +4,9 @@ package edu.bootcamp_sb.service_market.utill;
 
 public class UsernameSanitization {
 
+    private UsernameSanitization() {
+    }
+
     public static String sanitizeUsername(String unSanitizeUsername){
         return unSanitizeUsername.replace(" ", "_");
     }
