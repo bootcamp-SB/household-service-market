ac2fe956d9c64fee4bb8ad515815226943949bab
YasasSri2002 <srimedagedara234@gmail.com>
2026-01-08 11:39:54 +0530

    delete payment table

--- src/main/java/edu/bootcamp_sb/service_market/dto/BookingDto.java
+++ src/main/java/edu/bootcamp_sb/service_market/dto/BookingDto.java
@@ -1,7 +1,6 @@
 package edu.bootcamp_sb.service_market.dto;
 
 
-import edu.bootcamp_sb.service_market.entity.PaymentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
