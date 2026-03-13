b54ac18c3f3217cb988ab21962b52519ce7acfb8
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-13 17:14:35 +0530

    client persist test

--- src/test/java/service/ClientServiceTest.java
+++ src/test/java/service/ClientServiceTest.java
@@ -7,6 +7,7 @@ import edu.bootcamp_sb.service_market.exception.client_exceptions.ClientHasBeenN
 import edu.bootcamp_sb.service_market.repository.ClientRepository;
 import edu.bootcamp_sb.service_market.service.ClientService;
 import edu.bootcamp_sb.service_market.service.impl.ClientServiceImpl;
+import edu.bootcamp_sb.service_market.service.impl.KeyCloakUserHandleServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,6 +20,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
@@ -30,6 +32,9 @@ class ClientServiceTest {
 
     @InjectMocks
     private ClientServiceImpl clientService;
+
+    @InjectMocks
+    private KeyCloakUserHandleServiceImpl keyCloakUserHandleService;
 
 
 
@@ -44,7 +49,8 @@ class ClientServiceTest {
                 .thenReturn(Optional.of(mockClient));
 
         // Act
-        ResponseEntity<ClientResponseDto> clientServiceById = clientService.getById("f3c9b7a2-8d41-4e6e-9a73-2c5f1b8d4a9e");
+        ResponseEntity<ClientResponseDto> clientServiceById =
+                clientService.getById("f3c9b7a2-8d41-4e6e-9a73-2c5f1b8d4a9e");
 
         // Assert
         assertEquals("John", clientServiceById.getBody().getUsername());
@@ -54,12 +60,50 @@ class ClientServiceTest {
     @Test
     void testGetClientById_shouldNotExists(){
 
-        when(clientRepository.findById(UUID.fromString("1f9f3335-b1a1-45d2-b1e1-c406130708be")))
-                .thenReturn(Optional.empty());
+        when(clientRepository.findById(
+                    UUID.fromString("1f9f3335-b1a1-45d2-b1e1-c406130708be"))
+        ).thenReturn(Optional.empty());
 
         assertThrows(ClientHasBeenNotFoundException.class,
                 () -> clientService.getById("1f9f3335-b1a1-45d2-b1e1-c406130708be"));
 
     }
+
+    @Test
+    void testPersistClient(){
+
+        ClientRequestDto mockRequestDto = new ClientRequestDto();
+        mockRequestDto.setId(UUID.fromString("7ea7aab3-cf19-414d-8bc6-a1e8506ddcd3"));
+        mockRequestDto.setFirstName("ayrton");
+        mockRequestDto.setLastName("de silva");
+        mockRequestDto.setUsername("ayrton_senna");
+        mockRequestDto.setEmail("ayrton123@gmail.com");
+        mockRequestDto.setPassword("1234");
+
+        ClientEntity mockEntity = new ClientEntity();
+        mockEntity.setId(UUID.fromString("7ea7aab3-cf19-414d-8bc6-a1e8506ddcd3"));
+        mockEntity.setFirstName("ayrton");
+        mockEntity.setLastName("de silva");
+        mockEntity.setUsername("ayrton_senna");
+        mockEntity.setEmail("ayrton123@gmail.com");
+
+        when(keyCloakUserHandleService.createUser(mockRequestDto.getUsername()
+                , mockRequestDto.getLastName(),
+                mockRequestDto.getFirstName(),
+                mockRequestDto.getEmail(),
+                mockRequestDto.getPassword(),"client"));
+
+        when(clientRepository.save(mockEntity)).thenReturn(mockEntity);
+
+        ResponseEntity<ClientResponseDto> savedEntity = clientService.persist(mockRequestDto);
+
+        assertEquals(UUID.fromString("7ea7aab3-cf19-414d-8bc6-a1e8506ddcd3") ,
+                    savedEntity.getBody().getId());
+
+
+
+    }
+
+
 
 }
