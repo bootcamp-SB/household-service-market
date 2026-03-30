b94383e72b1dc91dd803bb74005f1d063b6130af
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-30 12:20:18 +0530

    fix: cancel booking api

--- src/main/java/edu/bootcamp_sb/service_market/controller/BookingController.java
+++ src/main/java/edu/bootcamp_sb/service_market/controller/BookingController.java
@@ -42,8 +42,8 @@ public class BookingController {
         return  bookingService.getBookingById(id);
     }
     @PutMapping("/cancel")
-    public ResponseEntity<Map<String,String>>cancelBooking(@RequestParam UUID taskId) throws MessagingException{
-        return bookingService.cancelBooking(taskId);
+    public ResponseEntity<Map<String,String>>cancelBooking(@RequestParam UUID bookingId) throws MessagingException{
+        return bookingService.cancelBooking(bookingId);
     }
 
     @GetMapping("/by-client-id")
